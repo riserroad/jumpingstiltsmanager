104cd2f1ece7de83a299186720db829386ed8e4c
Rémi <ponche62280@gmail.com>
2019-02-10 15:29:31 +0100

    test display in twig

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,15 +5,23 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Repository\JumpingTiltRepository;
+
+
 class MainController extends AbstractController
 {
     /**
      * @Route("/main", name="main")
      */
-    public function index()
+    public function index(JumpingTiltRepository $repo)
     {
+        $jumpingTilts = $repo->findAll() ; 
+
+        dump($jumpingTilts) ; 
+
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
+            'jumpingTilts' => $jumpingTilts, 
         ]);
     }
 }
